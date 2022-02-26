eeddb6a04553dd616b20a70bbb30148448b8902c
paulwababu <paulkiragu621@gmail.com>
2022-02-26 21:31:24 +0300

    more mods

--- pluto/urls.py
+++ pluto/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns = [
     path('', views.home),
+    path('simple_upload/', views.simple_upload, name='simple_upload'),
+    path('form_upload/', views.model_form_upload, name='model_form_upload'),
+    path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
