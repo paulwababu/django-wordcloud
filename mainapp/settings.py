313ed9f11371085922502fb5518318c569527173
paulwababu <paulkiragu621@gmail.com>
2022-02-28 17:36:54 +0300

    mods

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -12,10 +12,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+cloudinary.config( 
+  cloud_name = "prometheusapi", 
+  api_key = "138196782467569", 
+  api_secret = "0yyoct0-TFggpq3d_U_2A0nUn0o" 
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
--- pluto/views.py
+++ pluto/views.py
@@ -9,9 +9,10 @@ import re
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
-
+from .wordcloud import show
 from .models import Document
 from .forms import DocumentForm
+import os
 
 # Create your views here.
 def home(request):
@@ -20,13 +21,27 @@ def home(request):
 
 
 def simple_upload(request):
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        return render(request, 'simple_upload.html', {
-            'uploaded_file_url': uploaded_file_url
+    if request.method == 'POST' and request.FILES:
+        user_ip = request.META.get("REMOTE_ADDR")
+        if not os.path.exists('./whatsapp/uploaded_files/' + user_ip):
+            os.makedirs('./whatsapp/uploaded_files/' + user_ip)
+        path_to_upload = './whatsapp/uploaded_files/' + user_ip + "/"
+
+        files = request.FILES
+        for file in files.getlist('uploadedFile'):
+            if os.path.exists(path_to_upload + "chat.txt"):
+                os.remove(path_to_upload + "chat.txt")
+            with open(path_to_upload + "chat.txt", 'wb+') as f:
+                for chunk in file.chunks():
+                    f.write(chunk)
+        file_check = "Messages to this chat and calls are now secured with end-to-end encryption. Tap for more info."
+        file_check_group = "created group"
+        message = 'error'
+        with open(path_to_upload + "chat.txt", 'r', encoding="utf8") as f:
+            a = show(f)
+            print(a['secure_url'][54:])
+        return render(request, 'simple_upload_result.html', {
+            'uploaded_file_url': a['secure_url'][54:]
         })
     return render(request, 'simple_upload.html')
 
